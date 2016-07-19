cc40435bab3b306288b4078a541d1716d29c32de
MANJULA-05 <manjumaha.a@gmail.com>
2016-07-19 09:21:35 +0530

    Add files via upload
    hfj

---
+++ Fruit1.java
@@ -0,0 +1,14 @@
+package fruits;
+
+public class Fruit1 {
+	static String name;
+	static String taste;
+	static String size;
+	void eat(String ss,String jj)
+	{
+		name=ss;
+		taste=jj;
+		System.out.println("frt:"+ss+"\ntaste:"+jj);
+	}
+
+}
