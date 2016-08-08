79b21733efeab6d460aecf28dd23b3628495c9bf
MANJULA-05 <manjumaha.a@gmail.com>
2016-08-07 23:35:32 -0700

    Add files via upload
    pangram

---
+++ pangrams.java
@@ -0,0 +1,36 @@
+import java.io.*;
+import java.util.*;
+
+
+public class Solution {
+
+    public static final int m= 26; 
+
+    public int check(String arr){
+        if(arr.length() < m){
+           return -1;
+        }
+        for(char c ='A'; c <= 'Z' ; c++){
+            if((arr.indexOf(c) < 0) && (arr.indexOf((char)(c+32)) < 0)){
+               return -1;
+            }
+        }
+        return 1;
+    }
+
+
+    public static void main(String[] args) {
+
+        Scanner s1 = new Scanner(System.in);
+        String s = s1.next();
+        Solution obj = new Solution();
+
+        int d = obj.check(s);
+
+        if(d == -1)
+            System.out.print("not pangram");
+        else
+             System.out.print("pangram");
+
+    }
+}
